a265b6dd26dab23df40a6d46f6e67ea3e6cbb490
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2019-07-18 22:27:55 +0900

    First commit

---
+++ toDelete.js
@@ -0,0 +1,8 @@
+exports.posts = [
+  "http://localhost:2368/slated-for-deletion/",
+  "/another-slated-for-deletion",
+  "/dragon-quest-8",
+  "/dragon-quest-builders",
+  "/dragon-quest-builders-2",
+  "/dragon-quest-11"
+];
