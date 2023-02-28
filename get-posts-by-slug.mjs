e2df804c4c7540663c17f46e929c1120eea868d7
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2023-02-28 16:58:22 +0900

    fix: rename and move some variables

--- get-posts-by-slug.mjs
+++ get-posts-by-slug.mjs
@@ -6,24 +6,24 @@ const translationLang = 'espanol';
 const originalLang = 'english';
 const translationAPI = new GhostContentAPI({ ...keys[translationLang] });
 const originalAPI = new GhostContentAPI({ ...keys[originalLang] });
+const translatedPostSlugs = [
+  'que-es-una-cadena-de-caracteres-string-en-javascript',
+  'cual-es-la-direccion-ip-de-mi-router-como-encontrar-mi-direccion-wifi',
+  'como-obtener-la-direccion-ip-de-un-contenedor-docker-explicado-con-ejemplos'
+];
 
 const getPostsBySlug = async () => {
-  const translatedSlugs = [
-    'que-es-una-cadena-de-caracteres-string-en-javascript',
-    'cual-es-la-direccion-ip-de-mi-router-como-encontrar-mi-direccion-wifi',
-    'como-obtener-la-direccion-ip-de-un-contenedor-docker-explicado-con-ejemplos'
-  ];
   const translatedPosts = [];
   const originalPosts = [];
 
-  for (const translatedSlug of translatedSlugs) {
+  for (const translatedPostSlug of translatedPostSlugs) {
     const translatedPost = await translationAPI.posts.read({
       include: ['tags', 'authors'],
       filter: 'status:published',
-      slug: translatedSlug
+      slug: translatedPostSlug
     });
 
-    console.log(`Fetched translated post with slug: ${translatedSlug}...`);
+    console.log(`Fetched translated post with slug: ${translatedPostSlug}...`);
     translatedPosts.push(translatedPost);
     await pause(0.1);
   }
@@ -40,14 +40,14 @@ const getPostsBySlug = async () => {
     const match = originalPostRegex.exec(headAndFootCode);
     const { pathname } = new URL(match.groups.url);
     const pathSegments = pathname.split('/').filter(str => str);
-    const originalSlug = pathSegments[pathSegments.length - 1];
+    const originalPostSlug = pathSegments[pathSegments.length - 1];
     const originalPost = await originalAPI.posts.read({
       include: ['tags', 'authors'],
       filter: 'status:published',
-      slug: originalSlug
+      slug: originalPostSlug
     });
 
-    console.log(`Fetched original post with slug: ${originalSlug}...`);
+    console.log(`Fetched original post with slug: ${originalPostSlug}...`);
     originalPosts.push(originalPost);
     await pause(0.1);
   }
