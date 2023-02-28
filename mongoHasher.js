ff1ccdeca447a887ba664fe0615ddf64b0f17791
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2023-02-28 16:08:23 +0900

    chore: remove old scripts

--- mongoHasher.js
+++
@@ -1,9 +0,0 @@
-const getAllPosts = require('./mongoGetAllPosts.js');
-
-async function fetchPosts() {
-  const allPosts = await getAllPosts.constructIndex();
-
-  console.log(allPosts)
-}
-
-test();
